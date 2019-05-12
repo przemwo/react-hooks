24134a4c38fa4d57f7d5a13d8b093d8140e184fe
przemwo <przemwo@o2.pl>
2019-05-12 16:52:48 +0200

    changed App into functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ListWithHooks as List } from './ListWithHooks';
 // import { ListWithClass as List } from './ListWithClass';
 
 
-export default class App extends React.Component {
-    state = {
-        userLogin: "fmal",
-    };
-    handleUserIDChanged = (e) => {
-        this.setState({ userLogin: e.target.value });
+export const App = () => {
+    const [userLogin, setUserLogin] = useState("fmal");
+    const handleUserIDChanged = (e) => {
+        setUserLogin(e.target.value);
     }
-    render() {
-        return(
-            <div className="container">
-                <header>
-                    <nav>
-                        <div>
-                            <label htmlFor="fmal">Filip Malinowski</label>
-                            <input
-                                type="radio"
-                                id="fmal"
-                                name="userLogin"
-                                value="fmal"
-                                checked={this.state.userLogin == "fmal"}
-                                onChange={this.handleUserIDChanged}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="iamshaunjp">Shaun</label>
-                            <input
-                                type="radio"
-                                id="iamshaunjp"
-                                name="userLogin"
-                                value="iamshaunjp"
-                                checked={this.state.userLogin == "iamshaunjp"}
-                                onChange={this.handleUserIDChanged}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="gaearon">Dan Abramov</label>
-                            <input
-                                type="radio"
-                                id="gaearon"
-                                name="userLogin"
-                                value="gaearon"
-                                checked={this.state.userLogin == "gaearon"}
-                                onChange={this.handleUserIDChanged}
-                            />
-                        </div>
-                    </nav>
-                </header>
-                <List listID={this.state.userLogin} />
-            </div>
-        );
-    }
-}
+    return (
+        <div className="container">
+            <header>
+                <nav>
+                    <div>
+                        <label htmlFor="fmal">Filip Malinowski</label>
+                        <input
+                            type="radio"
+                            id="fmal"
+                            name="userLogin"
+                            value="fmal"
+                            checked={userLogin == "fmal"}
+                            onChange={handleUserIDChanged}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="iamshaunjp">Shaun</label>
+                        <input
+                            type="radio"
+                            id="iamshaunjp"
+                            name="userLogin"
+                            value="iamshaunjp"
+                            checked={userLogin == "iamshaunjp"}
+                            onChange={handleUserIDChanged}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="gaearon">Dan Abramov</label>
+                        <input
+                            type="radio"
+                            id="gaearon"
+                            name="userLogin"
+                            value="gaearon"
+                            checked={userLogin == "gaearon"}
+                            onChange={handleUserIDChanged}
+                        />
+                    </div>
+                </nav>
+            </header>
+            <List listID={userLogin} />
+        </div>
+    );
+};

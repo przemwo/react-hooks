b27be6a3da552e884950ab8208cf0a8925207a87
przemwo <przemwo@o2.pl>
2019-05-14 00:48:12 +0200

    customHooks

--- client/src/solutions/MyComponentHooks.js
+++ client/src/solutions/MyComponentHooks.js
@@ -1,44 +1,72 @@
 import React, { useState, useRef, useEffect } from 'react';
 
 
-export const MyComponentHooks = () => {
-    const [userName, setUserName] = useState("fmal");
+const useUserName = (name) => {
+    const [userName, setUserName] = useState(name);
     const handleChangeUserName = (e) => {
         setUserName(e.target.value);
     };
-    
+    return [
+        userName,
+        handleChangeUserName
+    ];
+};
+
+const useFocus = () => {
     const inputRef = useRef();
     useEffect(() => {
         inputRef.current.select();
     }, []);
-    
+    return inputRef;
+};
+
+const useWindowWidth = () => {
     const [windowWidth, setWindowWidth] = useState(0);
     useEffect(() => {
         const handleWindowResized = () => {
             console.log('resizing'); //shows importance of removing event listener on unmounting
             setWindowWidth(window.innerWidth);
         }
-        handleWindowResized();
         window.addEventListener('resize', handleWindowResized);
-        return () => window.removeEventListener('resize', handleWindowResized);
-    },[]);
-    
+        handleWindowResized();
+        return () => {
+            window.removeEventListener('resize', handleWindowResized);
+        };
+    }, []);
+    return windowWidth;
+};
+
+const useTimer = (userName) => {
     const [timer, setTimer] = useState(0);
     useEffect(() => {
-        const id = setInterval(() => {
+        setTimer(0);
+        const intervalID = setInterval(() => {
             console.log('tick');
             setTimer(timer => timer + 1);
         }, 1000);
         return () => {
-            setTimer(0);
-            clearInterval(id);
+            clearInterval(intervalID);
         };
     }, [userName]);
+    return timer;
+};
+
+export const MyComponentHooks = () => {
+    const [userName, handleChangeUserName] = useUserName("fmal");
+    const [userAge, handleChangeUserAge] = useUserName(30);
+
+    const inputRef = useFocus();
+
+    const windowWidth = useWindowWidth();
+
+    const timer = useTimer(userName);
+    const timerAge = useTimer(userAge);
 
     return (
         <div className="card">
             <h2>Width: {windowWidth}</h2>
             <h2>User name: <span>{userName}</span></h2>
+            <h2>User age: <span>{userAge}</span></h2>
             <input
                 ref={inputRef}
                 type="text"
@@ -48,6 +76,14 @@ export const MyComponentHooks = () => {
                 onChange={handleChangeUserName}
             />
             <h5>userName updated {timer}s ago</h5>
+            <input
+                type="text"
+                name="userAge"
+                id="userAge"
+                value={userAge}
+                onChange={handleChangeUserAge}
+            />
+            <h5>userAge updated {timerAge}s ago</h5>
         </div>
     );
 };
